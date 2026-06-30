713168b0b2c17e65a330c0aeb5cff5c262af57d9
0xwb7 <woobin200111@gmail.com>
2026-06-30 17:24:55 +0900

    feat: 로그인 페이지 구현 (#16)

---
+++ src/components/layout/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
+
+export default function ProtectedRoute({ children }) {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
